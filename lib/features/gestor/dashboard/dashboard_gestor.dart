9c7e60d50405fd5959ed96b536d244e5ed31dce7
felipetaua <taua.felipee@gmail.com>
2025-08-04 14:46:55 -0300

    criando o dashboard inical teste para empresa e gestor

---
+++ lib/features/gestor/dashboard/dashboard_gestor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GestorDashboardPage extends StatelessWidget {
+  const GestorDashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
