250f17b049014b8ee0c419fc00bf42eb70581824
felipetaua <taua.felipee@gmail.com>
2025-08-04 14:46:29 -0300

    criando telas de rotas para empresa teste

---
+++ lib/features/gestor/rotas/rotes_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RotesPages extends StatelessWidget {
+  const RotesPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
