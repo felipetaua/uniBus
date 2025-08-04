164ac336e34413726cb325875aedd731b6dc091f
felipetaua <taua.felipee@gmail.com>
2025-08-04 16:09:02 -0300

    adicionando a logo no topo

--- lib/features/estudante/message/message_screen.dart
+++ lib/features/estudante/message/message_screen.dart
@@ -82,6 +82,14 @@ class MessagePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            // Logo do Projeto
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 4.0),
+              child: Image.asset(
+                'assets/images/logo-texto-image.png',
+                height: 50,
+              ),
+            ),
             // Cabeçalho: "Chats" e ícones
             _buildHeader(),
 
@@ -107,7 +115,7 @@ class MessagePage extends StatelessWidget {
   // Widget para o cabeçalho
   Widget _buildHeader() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
