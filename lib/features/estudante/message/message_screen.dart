09b07bc6c4dc1c6d8b511799dfb66d02ce90dd83
felipetaua <taua.felipee@gmail.com>
2025-08-04 15:17:45 -0300

    alterando o textO

--- lib/features/estudante/message/message_screen.dart
+++ lib/features/estudante/message/message_screen.dart
@@ -5,6 +5,6 @@ class MessagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Página de Histórico')));
+    return const Scaffold(body: Center(child: Text('Página de Mensagens')));
   }
 }
